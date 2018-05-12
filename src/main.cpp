bd64efcd283f26c2931e2449f2fcd8231101a018
Fuzzbawls <fuzzbawls@gmail.com>
2018-05-11 22:19:32 -0700

    [Qt] Show progress percent for zpiv reindex operations
    `-reindexaccumulators` and `-reindexzerocoin` can take a considerable
    time to complete depending on system hardware. Lets show a progress percent
     similar to `VerifyDB()` on the splashscreen.
    
    Github-Pull: #612
    Rebased-From: 48e502aa2e9a22911f92eb0e5ea9422c6facf4c4

--- src/main.cpp
+++ src/main.cpp
@@ -2759,6 +2759,7 @@ bool RecalculatePIVSupply(int nHeightStart)
 bool ReindexAccumulators(list<uint256>& listMissingCheckpoints, string& strError)
 {
     // PIVX: recalculate Accumulator Checkpoints that failed to database properly
+    uiInterface.ShowProgress(_("Calculating missing accumulators..."), 0);
     if (!listMissingCheckpoints.empty() && chainActive.Height() >= Params().Zerocoin_StartHeight()) {
         LogPrintf("%s : finding missing checkpoints\n", __func__);
 
@@ -2768,6 +2769,8 @@ bool ReindexAccumulators(list<uint256>& listMissingCheckpoints, string& strError
         // find each checkpoint that is missing
         CBlockIndex* pindex = chainActive[nZerocoinStart];
         while (pindex) {
+            uiInterface.ShowProgress(_("Calculating missing accumulators..."), std::max(1, std::min(99, (int)((double)(pindex->nHeight - nZerocoinStart) / (double)(chainActive.Height() - nZerocoinStart) * 100))));
+
             if (ShutdownRequested())
                 return false;
 
@@ -2798,6 +2801,7 @@ bool ReindexAccumulators(list<uint256>& listMissingCheckpoints, string& strError
             }
             pindex = chainActive.Next(pindex);
         }
+        uiInterface.ShowProgress("", 100);
     }
     return true;
 }
