e47159bf6158c6bd80ec9ddf51e26ff8ab484c0f
Martin Mizzell <martin@bravelantern.com>
2018-10-07 11:51:24 -0500

    account for zerocoin during pow

--- src/primitives/transaction.h
+++ src/primitives/transaction.h
@@ -259,7 +259,10 @@ public:
 
     bool IsZerocoinSpend() const
     {
-        return (vin.size() > 0 && vin[0].prevout.hash == 0 && vin[0].scriptSig[0] == OP_ZEROCOINSPEND);
+        return (vin.size() > 0 &&
+            !vin[0].prevout.IsNull() &&
+            vin[0].prevout.hash == 0 &&
+            vin[0].scriptSig[0] == OP_ZEROCOINSPEND);
     }
 
     bool IsZerocoinMint() const
