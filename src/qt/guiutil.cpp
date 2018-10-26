0c038119d5418084b20ee2e0213617e6f752df81
Martin Mizzell <martin@bravelantern.com>
2018-10-26 03:18:30 -0500

    [#1] use valid address for UI placeholder

--- src/qt/guiutil.cpp
+++ src/qt/guiutil.cpp
@@ -102,7 +102,7 @@ void setupAddressWidget(QValidatedLineEdit* widget, QWidget* parent)
     widget->setFont(bitcoinAddressFont());
     // We don't want translators to use own addresses in translations
     // and this is the only place, where this address is supplied.
-    widget->setPlaceholderText(QObject::tr("Enter a PIVX address (e.g. %1)").arg("D7VFR83SQbiezrW72hjcWJtcfip5krte2Z"));
+    widget->setPlaceholderText(QObject::tr("Enter a PIVX address (e.g. %1)").arg("XaCWffkq3XKSgqGD8TfqFKrzoWKCDknCkV"));
     widget->setValidator(new BitcoinAddressEntryValidator(parent));
     widget->setCheckValidator(new BitcoinAddressCheckValidator(parent));
 }
