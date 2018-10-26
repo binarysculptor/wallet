cb321ef25e60ecf0899ea0a200e911fabce83ede
Martin Mizzell <martin@bravelantern.com>
2018-10-26 16:58:55 -0500

    [Branding] Pivx-Qt => Liberty-Qt

--- src/qt/macnotificationhandler.mm
+++ src/qt/macnotificationhandler.mm
@@ -14,7 +14,7 @@
 - (NSString *)__bundleIdentifier
 {
     if (self == [NSBundle mainBundle]) {
-        return @"io.pivx.Pivx-Qt";
+        return @"io.pivx.Liberty-Qt";
     } else {
         return [self __bundleIdentifier];
     }
@@ -65,7 +65,7 @@ MacNotificationHandler *MacNotificationHandler::instance()
     static MacNotificationHandler *s_instance = NULL;
     if (!s_instance) {
         s_instance = new MacNotificationHandler();
-        
+
         Class aPossibleClass = objc_getClass("NSBundle");
         if (aPossibleClass) {
             // change NSBundle -bundleIdentifier method to return a correct bundle identifier
