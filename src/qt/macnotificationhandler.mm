a066d97435c068ee50574902e01b1fffe6990a3a
Martin Mizzell <martin@bravelantern.com>
2018-10-26 17:06:06 -0500

    [Branding] io.pivx => com.liberty-coin

--- src/qt/macnotificationhandler.mm
+++ src/qt/macnotificationhandler.mm
@@ -14,7 +14,7 @@
 - (NSString *)__bundleIdentifier
 {
     if (self == [NSBundle mainBundle]) {
-        return @"io.pivx.Liberty-Qt";
+        return @"com.liberty-coin.Liberty-Qt";
     } else {
         return [self __bundleIdentifier];
     }
