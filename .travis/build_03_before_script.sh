810282341eb3fc179d056f67cd6b62f8197fb80c
gdiscord <gnetbusy@gmail.com>
2019-01-28 20:24:58 +0000

    check ownership 3

--- .travis/build_03_before_script.sh
+++ .travis/build_03_before_script.sh
@@ -20,6 +20,7 @@ if [[ $HOST = *-mingw32 ]]; then
   DOCKER_EXEC update-alternatives --set $HOST-g++ \$\(which $HOST-g++-posix\)
 fi
 if [ -z "$NO_DEPENDS" ]; then
+  echo "script03 NO_DEPENDS - CONFIG_SHELL= make"
   DOCKER_EXEC CONFIG_SHELL= make $MAKEJOBS -C depends HOST=$HOST $DEP_OPTS
 fi
 
