0c06756760512cfe24f607ed5e9372b972419868
gdiscord <gnetbusy@gmail.com>
2019-01-28 02:08:27 +0000

    list files.

--- .travis/build_04_script.sh
+++ .travis/build_04_script.sh
@@ -5,10 +5,16 @@
 # file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
 export LC_ALL=C.UTF-8
-chown -R travis:travis /home/travis/
 
 TRAVIS_COMMIT_LOG=$(git log --format=fuller -1)
 export TRAVIS_COMMIT_LOG
+
+DOCKER_EXEC pwd
+pwd
+ls -la
+cd ..
+DOCKER_EXEC pwd
+ls -la
 
 OUTDIR=$BASE_OUTDIR/$TRAVIS_PULL_REQUEST/$TRAVIS_JOB_NUMBER-$HOST
 BITCOIN_CONFIG_ALL="--disable-dependency-tracking --prefix=$TRAVIS_BUILD_DIR/depends/$HOST --bindir=$OUTDIR/bin --libdir=$OUTDIR/lib"
