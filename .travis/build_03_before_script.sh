2aae818ca232b6f5e07364681570f3d435bba76a
gdiscord <gnetbusy@gmail.com>
2019-01-29 13:30:45 +0000

    mkdir -p depends

--- .travis/build_03_before_script.sh
+++ .travis/build_03_before_script.sh
@@ -8,7 +8,7 @@ export LC_ALL=C.UTF-8
 
 #DOCKER_EXEC echo \> \$HOME/.pivx  # Make sure default datadir does not exist and is never read by creating a dummy file
 
-DOCKER_EXEC su travis -c  "mkdir -p depends/SDKs depends/sdk-sources"
+DOCKER_EXEC mkdir -p depends/SDKs depends/sdk-sources
 
 if [ -n "$OSX_SDK" -a ! -f depends/sdk-sources/MacOSX${OSX_SDK}.sdk.tar.gz ]; then
   curl --location --fail $SDK_URL/MacOSX${OSX_SDK}.sdk.tar.gz -o depends/sdk-sources/MacOSX${OSX_SDK}.sdk.tar.gz
