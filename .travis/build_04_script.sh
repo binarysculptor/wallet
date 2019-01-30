c82f7a3054cd6d1ff53626f13e85ec133e88c551
gdiscord <gnetbusy@gmail.com>
2019-01-30 23:20:19 +0000

    make using travis_xx_logs_1

--- .travis/build_04_script.sh
+++ .travis/build_04_script.sh
@@ -44,7 +44,11 @@ ls -la ../
 echo "ls -la ../../ end listing"
 ls -la ../../
 
-su travis -c  "mkdir build"
+pwd
+DOCKER_EXEC pwd
+
+sudo -u travis "mkdir build"
+
 chown -R 1000:1000 build
 #DOCKER_EXEC "sudo -u \#1000 'mkdir build'"
 cd build || (echo "could not enter build directory"; exit 1)
