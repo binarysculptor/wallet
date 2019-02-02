021ad4faa2c569bf23fe347e4b636d95c9ed627e
gdiscord <gnetbusy@gmail.com>
2019-02-02 04:13:29 +0000

    Rebuild the build system

---
+++ .travis/build_02_install.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+#
+# Copyright (c) 2018 The Bitcoin Core developers
+# Distributed under the MIT software license, see the accompanying
+# file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+export LC_ALL=C.UTF-8
+
+travis_retry docker pull "$DOCKER_NAME_TAG"
+env | grep -E '^(CCACHE_|WINEDEBUG|LC_ALL|BOOST_TEST_RANDOM|CONFIG_SHELL)' | tee /tmp/env
+if [[ $HOST = *-mingw32 ]]; then
+  DOCKER_ADMIN="--cap-add SYS_ADMIN"
+fi
+DOCKER_ID=$(docker run $DOCKER_ADMIN -idt --mount type=bind,src=$TRAVIS_BUILD_DIR,dst=$TRAVIS_BUILD_DIR --mount type=bind,src=$CCACHE_DIR,dst=$CCACHE_DIR -w $TRAVIS_BUILD_DIR --env-file /tmp/env $DOCKER_NAME_TAG)
+
+DOCKER_EXEC () {
+  docker exec $DOCKER_ID bash -c "cd $PWD && $*"
+}
+
+if [ -n "$DPKG_ADD_ARCH" ]; then
+  DOCKER_EXEC dpkg --add-architecture "$DPKG_ADD_ARCH"
+fi
+
+mkdir -p /home/travis/build/project-liberty/wallet/build/
+
+#DOCKER_EXEC usermod -aG travis
+cat /etc/passwd
+travis_retry DOCKER_EXEC apt-get update
+travis_retry DOCKER_EXEC apt-get -y install software-properties-common
+travis_retry DOCKER_EXEC apt-get update
+travis_retry DOCKER_EXEC add-apt-repository ppa:bitcoin/bitcoin
+travis_retry DOCKER_EXEC apt-get update
+travis_retry DOCKER_EXEC apt-get -y install libdb4.8-dev libdb4.8++-dev libboost-all-dev libssl1.0-dev libevent-dev libgmp3-dev libzmq3-dev doxygen autoconf
+travis_retry DOCKER_EXEC apt-get -y install sudo
+#
+travis_retry DOCKER_EXEC useradd -m -s /bin/bash travis
+DOCKER_EXEC adduser travis sudo
+travis_retry DOCKER_EXEC chown -R travis:travis /home/travis
+travis_retry DOCKER_EXEC apt-get install --no-install-recommends --no-upgrade -qq $PACKAGES $DOCKER_PACKAGES
---
+++ .travis/build_04_script.sh
@@ -0,0 +1,116 @@
+#!/usr/bin/env bash
+#
+# Copyright (c) 2018 The Bitcoin Core developers
+# Distributed under the MIT software license, see the accompanying
+# file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+export LC_ALL=C.UTF-8
+
+TRAVIS_COMMIT_LOG=$(git log --format=fuller -1)
+export TRAVIS_COMMIT_LOG
+
+OUTDIR=$BASE_OUTDIR/$TRAVIS_PULL_REQUEST/$TRAVIS_JOB_NUMBER-$HOST
+BITCOIN_CONFIG_ALL="--disable-dependency-tracking --prefix=$TRAVIS_BUILD_DIR/depends/$HOST --bindir=$OUTDIR/bin --libdir=$OUTDIR/lib"
+if [ -z "$NO_DEPENDS" ]; then
+  DOCKER_EXEC ccache --max-size=$CCACHE_SIZE
+fi
+
+BEGIN_FOLD autogen
+  if [ -n "$CONFIG_SHELL" ]; then
+    DOCKER_EXEC $CONFIG_SHELL -c ./autogen.sh
+  else
+    DOCKER_EXEC "su travis -c ./autogen.sh"
+  fi
+END_FOLD
+#DOCKER_EXEC "su travis -c './autoreconf -f'"
+
+DOCKER_EXEC pwd
+DOCKER_EXEC ls -la
+#DOCKER_EXEC "su travis -c 'autoreconf --force --install'"
+
+
+cd build || (echo "could not enter build directory"; exit 1)
+
+BEGIN_FOLD configure
+   DOCKER_EXEC 'su travis -c "../configure --cache-file=config.cache $BITCOIN_CONFIG_ALL $BITCOIN_CONFIG || ( cat config.log && false)"'
+   #DOCKER_EXEC "cat config.log"
+   #DOCKER_EXEC cd ..
+   DOCKER_EXEC pwd
+   ls -la
+   #DOCKER_EXEC "su travis -c 'autoreconf --force --install'"
+   #DOCKER_EXEC 'su travis -c "./configure --cache-file=config.cache $BITCOIN_CONFIG_ALL $BITCOIN_CONFIG || ( cat config.log && false)"'
+   #DOCKER_EXEC "cat config.cache"
+END_FOLD
+
+
+pwd
+ls -la
+echo "build/src"
+ls -la ./src
+ls -la ../
+
+
+echo "find /homeMakefile src/Makefile doc/man/Makefile share/setup.nsi share/qt/Info.plist test/config.ini"
+DOCKER_EXEC "find /home/travis  -name 'Makefile'"
+DOCKER_EXEC "find /home/travis  -name 'config.ini'"
+DOCKER_EXEC "find /home/travis  -name 'Info.plist'"
+DOCKER_EXEC "find /home/travis  -name 'setup.nsi'"
+#find /home/travis/build/project-liberty/wallet -name "makefile" -maxdepth 3
+
+BEGIN_FOLD distdir
+   #DOCKER_EXEC "su travis -c 'make VERSION=$HOST'"
+END_FOLD
+
+cd "liberty-$HOST" || (echo "could not enter distdir liberty-$HOST"; exit 1)
+pwd
+BEGIN_FOLD configure
+  DOCKER_EXEC 'su travis -c "./configure --cache-file=../config.cache $BITCOIN_CONFIG_ALL $BITCOIN_CONFIG || ( cat config.log && false)"'
+END_FOLD
+pwd
+ls -la
+echo "2. config.status"
+
+
+DOCKER_EXEC "find /home/travis/build/project-liberty/wallet  -name 'Makefile'"
+DOCKER_EXEC "find /home/travis/build/project-liberty/wallet  -name 'config.status'"
+DOCKER_EXEC "find /home/travis/build/project-liberty/wallet  -name 'make'"
+#DOCKER_EXEC "find /home/travis/build/project-liberty/wallet -maxdepth 3 -name 'Makefile'"
+#DOCKER_EXEC "find /home/travis/build/project-liberty/wallet -maxdepth 3 -name 'makefile'"
+#DOCKER_EXEC "find /home/travis/build  -maxdepth 5 -name 'config.cache'" >> found /home/travis/build/project-liberty/config.cache
+echo "ls -la /home/travis"
+DOCKER_EXEC "ls -la /home/travis"
+echo "ls -la /home/travis/build"
+DOCKER_EXEC "ls -la /home/travis/build"
+echo "ls -la /home/travis/build/project-liberty/"
+DOCKER_EXEC "ls -la /home/travis/build/project-liberty/"
+config.cache
+
+BEGIN_FOLD build
+    DOCKER_EXEC "su travis -c 'make $MAKEJOBS $GOAL'"
+    #DOCKER_EXEC "su travis -c 'make -C /home/travis/build/project-liberty/wallet/depends $MAKEJOBS $GOAL'"
+    #DOCKER_EXEC "su travis -c 'make $MAKEJOBS'"
+    #DOCKER_EXEC "su travis -c 'make $GOAL V=1 ; false )'"
+    DOCKER_EXEC "su travis -c 'make $GOAL V=1 ; false )'"
+END_FOLD
+
+if [ "$RUN_UNIT_TESTS" = "true" ]; then
+  BEGIN_FOLD unit-tests
+  #DOCKER_EXEC LD_LIBRARY_PATH=$TRAVIS_BUILD_DIR/depends/$HOST/lib make $MAKEJOBS check VERBOSE=1
+  END_FOLD
+fi
+
+if [ "$RUN_BENCH" = "true" ]; then
+  BEGIN_FOLD bench
+  #DOCKER_EXEC LD_LIBRARY_PATH=$TRAVIS_BUILD_DIR/depends/$HOST/lib $OUTDIR/bin/bench_liberty -scaling=0.001
+  END_FOLD
+fi
+
+if [ "$TRAVIS_EVENT_TYPE" = "cron" ]; then
+  extended="--extended --exclude feature_pruning,feature_dbcrash"
+fi
+
+if [ "$RUN_FUNCTIONAL_TESTS" = "true" ]; then
+  BEGIN_FOLD functional-tests
+  #DOCKER_EXEC test/functional/test_runner.py --combinedlogslen=4000 --coverage --quiet --failfast ${extended}
+  END_FOLD
+fi
